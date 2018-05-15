93efb497d8670bb28ccbbe250f88d2d7bb058d1c
yilmazumut <32803270+yilmazumut@users.noreply.github.com>
2018-05-15 10:04:58 +0300

    Update sql.h
    Single and Double search functions were added. These are template functions so they need to perfect match with struct name,author and book in tmp.h file, otherwise they give an error.

--- sql.h
+++ sql.h
@@ -1,4 +1,4 @@
-// version 1.0  14/05/2018
+// version 1.1  15/05/2018
 
 #ifndef SQL_H
 #define SQL_H
@@ -6,6 +6,7 @@
 #include <QString>
 #include <QSqlDatabase>
 #include <QSqlQuery>
+#include "tmp.h"
 
 class LibMngSys{
 private:
@@ -23,7 +24,60 @@ public:
   bool  deleteData (QString name, QString author, QString year);
   bool  saveChanges();
 
+  template <typename T>
+  QList<QList<QString> >  singleSearch (QString key);
+
+  template <typename T1,typename T2>
+  QList<QList<QString> >  doubleSearch (QString key1, QString key2);
+  
   QList<QList<QString> >  getAllData   ();
 };
+
+template <typename T>
+QList<QList<QString> > LibMngSys::singleSearch(QString key){
+  query.prepare( singleSearchParam<T>::text );
+  query.bindValue(":key",key);
+  QList<QList<QString> > stringList;
+  if(query.exec()){
+      while(query.next()){
+          QList<QString> list;
+          list.append(query.value(0).toString());
+          list.append(query.value(1).toString());
+          list.append(query.value(2).toString());
+          list.append(query.value(3).toString());
+          list.append(query.value(4).toString());
+          stringList.append(list);
+        }
+      return stringList;
+    }
+  else{
+      qDebug() << "Query execution error.";
+      return stringList;
+    }
+}
+
+template <typename T1, typename T2>
+QList<QList<QString> > LibMngSys::doubleSearch(QString key1, QString key2){
+  query.prepare( doubleSearchParam<T1,T2>::text );
+  query.bindValue(":key1",key1);
+  query.bindValue(":key2",key2);
+  QList<QList<QString> > stringList;
+  if(query.exec()){
+      while(query.next()){
+          QList<QString> list;
+          list.append(query.value(0).toString());
+          list.append(query.value(1).toString());
+          list.append(query.value(2).toString());
+          list.append(query.value(3).toString());
+          list.append(query.value(4).toString());
+          stringList.append(list);
+        }
+      return stringList;
+    }
+  else{
+      qDebug() << "Query execution error.";
+      return stringList;
+    }
+}
 
 #endif // SQL_H
